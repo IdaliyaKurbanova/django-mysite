395784709b984e6b644cf6265e6bf57040edfdfb
Идалия Курбанова <bulgak.417@mail.ru>
2023-11-11 01:53:52 +0300

    django-templates with tags and filters were added to shopapp

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,21 +1,21 @@
 from timeit import default_timer
 from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
+from typing import List, Dict, Any
 
 
 def shop_index(request: HttpRequest):
-    products = [('lipstick', 599, 0, 0),
+    products: List[tuple] = [('lipstick', 599, 0, 0),
                 ('eyeshadows', 999, 20, datetime.strptime("Nov 15 2023 1:33PM", "%b %d %Y %I:%M%p")),
                 ('powder', 1199, 0, 0),
                 ('perfume', 2599, 0, 0),
                 ('eyeliner', 49, 0, 0),
                 ]
-    product_names = [item[0] for item in products]
-    context = {
+    product_names: List[str] = [item[0] for item in products]
+    context: Dict[str, Any] = {
         'launch_time': default_timer(),
         'products': products,
         'product_names': product_names,
     }
-    return HttpResponse("Hello, world!")
-    # return render(request, 'shopapp/shopapp-index.html', context=context)
+    return render(request, 'shopapp/shopapp-index.html', context=context)
