0a1b28bf56c637c9225883ae4fdddf043d54cdf9
Идалия Курбанова <bulgak.417@mail.ru>
2023-11-16 11:22:49 +0300

    model Product and appropriate migration were created

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount = models.PositiveSmallIntegerField(default=0, blank=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+
