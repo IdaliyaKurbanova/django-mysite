06781ea406e9559b16436d09f2a529d99aed8f85
Идалия Курбанова <bulgak.417@mail.ru>
2023-11-16 21:53:09 +0300

    view-functions for orders-list.html, products-list.html were added

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,21 +1,27 @@
-from timeit import default_timer
-from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 from typing import List, Dict, Any
+from .models import Product, Order
 
 
 def shop_index(request: HttpRequest):
-    products: List[tuple] = [('lipstick', 599, 0, 0),
-                ('eyeshadows', 999, 20, datetime.strptime("Nov 15 2023 1:33PM", "%b %d %Y %I:%M%p")),
-                ('powder', 1199, 0, 0),
-                ('perfume', 2599, 0, 0),
-                ('eyeliner', 49, 0, 0),
-                ]
-    product_names: List[str] = [item[0] for item in products]
-    context: Dict[str, Any] = {
-        'launch_time': default_timer(),
-        'products': products,
-        'product_names': product_names,
-    }
+    context: Dict[str, List[Dict[str]]] = {
+    "menu": [{'title': "Products to order", 'url_name': 'shopapp:products_list'},
+            {'title': "Current orders list", 'url_name': 'shopapp:orders_list'},],
+                             }
     return render(request, 'shopapp/shopapp-index.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context: Dict[str, Any] = {
+    "products": Product.objects.all()
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context: Dict[str, Any] = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all()
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
+
