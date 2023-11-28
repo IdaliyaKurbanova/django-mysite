86fd9b50b8c55afbecb3a29a699f0ffd17be476e
Идалия Курбанова <bulgak.417@mail.ru>
2023-11-28 12:23:09 +0300

    throttling middleware was added to project

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'requestdataapp.middlewares.Throttling_Middleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
