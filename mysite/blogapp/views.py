d3082372b99074e3bbfa2cee2ce9257562119a02
Идалия Курбанова <bulgak.417@mail.ru>
2024-01-12 02:02:04 +0300

    new blogapp was added to the project

--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Article
+import datetime
 
 
 class ArticleListView(ListView):
-    queryset = Article.objects.select_related('author, category').prefetch_related('tags').defer('content')
+    queryset = (
+        Article.objects.defer('content').select_related('author').
+        select_related('category').prefetch_related('tags'))
+
--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LogoutView
@@ -17,11 +18,16 @@ class MyLogoutView(LogoutView):
 class RegistrationView(CreateView):
     form_class = UserCreationForm
     template_name = 'myauth/register.html'
-    success_url = reverse_lazy('myauth:login')
+    success_url = reverse_lazy('myauth:about_me')
 
     def form_valid(self, form):
         response = super().form_valid(form)
         Profile.objects.create(user=self.object)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password2')
+        user = authenticate(request=self.request, username=username, password=password)
+        login(request=self.request, user=user)
+
         return response
 
 
